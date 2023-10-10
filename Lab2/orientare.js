1b43267cdfdc3bfbf7f74de083b39398ed007a12
Bosna28 <bosna28vk@mail.ru>
2023-10-10 20:44:58 +0300

    exp14

--- Lab2/orientare.js
+++ Lab2/orientare.js
@@ -11,16 +11,16 @@ document.getElementById("b").innerHTML = "beta = "+beta;
 document.getElementById("c").innerHTML = "gamma = "+gamma;
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var centru = {x:canvas.width/2, y:canvas.height/2}
+//var centru = {x:canvas.width/2, y:canvas.height/2}
 var latura_patrat = 50;
 //context.setTransform(1, 0, 0, 1, 0, 0);
 context.clearRect(0,0,canvas.width, canvas.height);
 //context.translate(centru.x/2, centru.y/2);
 //context.rotate(alpha * Math.PI / 180);
-context.beginPath();
+//context.beginPath();
 //context.fillStyle = "#FF0000";
 //context.rect(centru.x,centru.y,latura_patrat*Math.abs(gamma/180),latura_patrat*Math.abs(gamma/180));
-context.rect(canvas.width/2,canvas.height/2,
+context.rect(0,0,
 300,300);
 /* context.rect(centru.x/2,centru.y/2, 
 latura_patrat*(+Math.abs(gamma/180))%canvas.width/2, latura_patrat*(+Math.abs(gamma/180))%canvas.height/2 ); */
