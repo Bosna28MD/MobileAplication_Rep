cede03814eda379b8b0745e52237669ed0d78094
Bosna28 <bosna28vk@mail.ru>
2023-10-10 20:23:42 +0300

    exp8

--- Lab2/orientare.js
+++ Lab2/orientare.js
@@ -15,11 +15,11 @@ var centru = {x:canvas.width/2, y:canvas.height/2}
 var latura_patrat = 50;
 context.setTransform(1, 0, 0, 1, 0, 0);
 context.clearRect(0,0,canvas.width, canvas.height);
-//context.translate(centru.x/2, centru.y/2);
+context.translate(centru.x/2, centru.y/2);
 //context.rotate(alpha * Math.PI / 180);
 context.beginPath();
 context.fillStyle = "#FF0000";
-context.rect(canvas.width/2-latura_patrat,canvas.height/2-latura_patrat, 
+context.rect(centru.x/2,centru.y/2, 
 latura_patrat*(+Math.abs(gamma/180))%canvas.width/2, latura_patrat*(+Math.abs(gamma/180))%canvas.height/2 );
 context.stroke();
 }
