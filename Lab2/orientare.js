f12978a8376b74200d9dcb95ebcd506505c78c08
Bosna28 <bosna28vk@mail.ru>
2023-10-10 22:24:49 +0300

    try-1

--- Lab2/orientare.js
+++ Lab2/orientare.js
@@ -1,6 +1,5 @@
 //Comment...
-window.addEventListener("deviceorientation", 
-on_device_orientation);
+
 function on_device_orientation(evt)
 {
 var alpha = evt.alpha;
@@ -11,20 +10,26 @@ document.getElementById("b").innerHTML = "beta = "+beta;
 document.getElementById("c").innerHTML = "gamma = "+gamma;
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+context.fillStyle = "red";
 //var centru = {x:canvas.width/2, y:canvas.height/2}
-var latura_patrat = 50;
+var latura_patrat = 40;
 //context.setTransform(1, 0, 0, 1, 0, 0);
 context.clearRect(0,0,canvas.width, canvas.height);
+//context.fillStyle = "red";
 //context.translate(centru.x/2, centru.y/2);
 //context.rotate(alpha * Math.PI / 180);
-//context.beginPath();
-//context.fillStyle = "#FF0000";
-//context.rect(centru.x,centru.y,latura_patrat*Math.abs(gamma/180),latura_patrat*Math.abs(gamma/180));
+context.beginPath();
 context.fillStyle = "#FF0000";
-context.rect(0,0,
-300,300);
+//context.rect(centru.x,centru.y,latura_patrat*Math.abs(gamma/180),latura_patrat*Math.abs(gamma/180));
+
+context.fillRect(canvas.width/2,canvas.height/2,latura_patrat+gamma,latura_patrat+gamma);
+
+
 /* context.rect(centru.x/2,centru.y/2, 
 latura_patrat*(+Math.abs(gamma/180))%canvas.width/2, latura_patrat*(+Math.abs(gamma/180))%canvas.height/2 ); */
 
 context.stroke();
 }
+
+window.addEventListener("deviceorientation", 
+on_device_orientation);
